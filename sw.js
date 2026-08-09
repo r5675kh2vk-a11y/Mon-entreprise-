aa9df892d0a8207bedc561e64f4251bc3afb7647
r5675kh2vk-a11y <r5675kh2vk@privaterelay.appleid.com>
2026-08-09 14:56:18 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-const C='me-v3';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./logo-mon-entreprise.jpg','./icon-192.png','./icon-512.png','./saphir.jpg','./mysilae.jpg','./edocperso.jpg','./luminiz.jpg','./waze.jpg','./maps.jpg','./campus.jpg','./unice.jpg']))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+const CACHE='me-v5';
+const ASSETS=["./", "./index.html", "./manifest.webmanifest", "./logo-mon-entreprise.jpg", "./icon-192.png", "./icon-512.png", "./saphir.jpg", "./luminiz.jpg", "./waze.jpg", "./maps.jpg", "./mysilae.jpg", "./edocperso.jpg", "./campus.jpg", "./unice.jpg"];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return r;}).catch(()=>caches.match(e.request)));});
