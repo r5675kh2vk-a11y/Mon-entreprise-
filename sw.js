26ebbbedb088dd4274db676917494d5cd6fa0928
r5675kh2vk-a11y <r5675kh2vk@privaterelay.appleid.com>
2026-08-09 15:15:46 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='me-v5';
+const CACHE='me-v52';
 const ASSETS=["./", "./index.html", "./manifest.webmanifest", "./logo-mon-entreprise.jpg", "./icon-192.png", "./icon-512.png", "./saphir.jpg", "./luminiz.jpg", "./waze.jpg", "./maps.jpg", "./mysilae.jpg", "./edocperso.jpg", "./campus.jpg", "./unice.jpg"];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return r;}).catch(()=>caches.match(e.request)));});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return; e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));});
